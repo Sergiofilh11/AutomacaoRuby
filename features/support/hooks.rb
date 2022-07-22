dbbe35b41afebd4ba6bcac1227f6880a3f83efb7
Sérgio Filho <fragasergio23@gmail.com>
2022-07-21 22:10:27 -0300

    Add file page_helper.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,10 @@
-# Excluir os Cookies antes dos testes
+# Excluir os Cookies antes da execução
 Before do |scenario|
     Capybara.current_session.driver.manager.delete_all_cookies
     page.driver.quit
 end
 
-
+# Depois da execução tirar um print
 After do |scenario|
     scenario_name = scenario.name.gsub(/\s+/, '_').tr('/','_')
 
