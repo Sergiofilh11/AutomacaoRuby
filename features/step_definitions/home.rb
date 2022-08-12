588d00cdf89a74c419dd1bb869df7ae810aa21fc
Sérgio Filho <fragasergio23@gmail.com>
2022-08-12 10:06:41 -0300

    Finalizing config home.rb

--- features/step_definitions/home.rb
+++ features/step_definitions/home.rb
@@ -2,6 +2,10 @@ Dado('que eu acesse a Home Page do projeto Qa.Coders') do
     home.load
 end
   
-Então('devo visualizar a informação {string}') do |titleHomePage|
-    home.validate_text_homePage(titleHomePage)    
+Então('devo visualizar a informação {string}') do |text_home_feature|
+    home.validate_text_home(text_home_feature)    
+end
+
+Quando('clico no menu {string}') do |menu|
+    home.click_button(menu)
 end
